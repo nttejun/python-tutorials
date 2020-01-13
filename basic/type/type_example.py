e8d5d160f5f761f695ee36cffdef316da5041a30
nttejun <nttejun@gmail.com>
2020-01-14 07:30:38 +0900

    1. 자료형과 문자열

---
+++ basic/type/type_example.py
@@ -0,0 +1,44 @@
+import keyword
+print(keyword.kwlist)
+print(52)
+print("")
+print("test")
+print('test')
+print()
+print(type("test"))
+print("'test'입니다")
+print("\"test\"입니다")
+print("사는 곳\t나이\t이름\t사는곳\t경기도고양시\t이상")
+print("가\t가나\t가나다\t가나다라\t가나다라마\t가나다라마바")
+print("\\ \\ \\ \\ \\")
+print(
+"""이름
+나이
+사는곳"""
+)
+print(
+"""\
+이름
+나이
+사는곳\
+"""
+)
+print(
+"""
+이름
+나이
+사는곳
+"""
+)
+print("3회 반복\n" * 3)
+print("테스트"[0])
+print("테스트"[1])
+print("테스트"[2])
+print("테스트"[-1])
+print("테스트"[-2])
+print("테스트"[-3])
+print("파이썬연습"[0:1])
+print("파이썬연습"[2:3])
+print("파이썬연습"[2:4])
+print("테스트".split("스"))
+print(len("파이썬연습"))
